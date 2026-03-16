c578b28d1c783cd21806a1bddd9122bbd5c28ccd
ArbazAnsari10 <arbaz.ansari@cmptechllp.com>
2026-03-16 12:35:27 +0530

    map

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/Admin/Layout';
 import Dashboard from './components/Admin/Dashboard/Dashboard';
 import Factory from './components/Admin/Factories/Factory';
+import FactoryNetworkMap from './components/Admin/Map/FactoryNetworkMap';
 
 const App = () => {
   return (
@@ -13,6 +14,8 @@ const App = () => {
 
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="factories" element={<Factory />} />
+          <Route path="map" element={<FactoryNetworkMap />} />
+          
 
         </Route>
       </Routes>
